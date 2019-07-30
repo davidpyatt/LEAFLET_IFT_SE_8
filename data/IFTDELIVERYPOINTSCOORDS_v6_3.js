b6a3100a87157722875a40acb8b888a22b673dfa
davidpyatt <52712354+davidpyatt@users.noreply.github.com>
2019-07-30 16:31:04 +0100

    Add files via upload

---
+++ data/IFTDELIVERYPOINTSCOORDS_v6_3.js
@@ -0,0 +1,12 @@
+var json_IFTDELIVERYPOINTSCOORDS_v6_3 = {
+"type": "FeatureCollection",
+"name": "IFTDELIVERYPOINTSCOORDS_v6_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "City 2": "Canterbury", "LAT 1": 51.2802, "LONG 1": 1.0789, "City 2_1": "Canterbury", "LAT 1_1": 51.2802, "LONG 1_1": 1.0789 }, "geometry": { "type": "Point", "coordinates": [ 1.0789, 51.2802 ] } },
+{ "type": "Feature", "properties": { "City 2": "Gravesend", "LAT 1": 51.4419, "LONG 1": 0.3708, "City 2_1": "Gravesend", "LAT 1_1": 51.4419, "LONG 1_1": 0.3708 }, "geometry": { "type": "Point", "coordinates": [ 0.3708, 51.4419 ] } },
+{ "type": "Feature", "properties": { "City 2": "Hove", "LAT 1": 50.8279, "LONG 1": -0.1687, "City 2_1": "Hove", "LAT 1_1": 50.8279, "LONG 1_1": -0.1687 }, "geometry": { "type": "Point", "coordinates": [ -0.1687, 50.8279 ] } },
+{ "type": "Feature", "properties": { "City 2": "Portsmouth", "LAT 1": 50.8198, "LONG 1": -1.088, "City 2_1": "Portsmouth", "LAT 1_1": 50.8198, "LONG 1_1": -1.088 }, "geometry": { "type": "Point", "coordinates": [ -1.088, 50.8198 ] } },
+{ "type": "Feature", "properties": { "City 2": "Milton Keynes", "LAT 1": 52.0406, "LONG 1": -0.7594, "City 2_1": "Milton Keynes", "LAT 1_1": 52.0406, "LONG 1_1": -0.7594 }, "geometry": { "type": "Point", "coordinates": [ -0.7594, 52.0406 ] } }
+]
+}
---
+++ data/IFTDELIVERYPOINTS_v7_2.js
@@ -0,0 +1,12 @@
+var json_IFTDELIVERYPOINTS_v7_2 = {
+"type": "FeatureCollection",
+"name": "IFTDELIVERYPOINTS_v7_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "City 1": "Maidstone", "LAT": 51.2704, "LONG": 0.5227, "field_4": null, "field_5": "  " }, "geometry": { "type": "Point", "coordinates": [ 0.5227, 51.2704 ] } },
+{ "type": "Feature", "properties": { "City 1": "Southampton", "LAT": 50.9097, "LONG": -1.4044, "field_4": null, "field_5": " " }, "geometry": { "type": "Point", "coordinates": [ -1.4044, 50.9097 ] } },
+{ "type": "Feature", "properties": { "City 1": "Bournemouth", "LAT": 50.7192, "LONG": -1.8808, "field_4": null, "field_5": " " }, "geometry": { "type": "Point", "coordinates": [ -1.8808, 50.7192 ] } },
+{ "type": "Feature", "properties": { "City 1": "Abingdon", "LAT": 51.6708, "LONG": -1.288, "field_4": null, "field_5": " " }, "geometry": { "type": "Point", "coordinates": [ -1.288, 51.6708 ] } },
+{ "type": "Feature", "properties": { "City 1": "Reading", "LAT": 51.4543, "LONG": -0.9781, "field_4": null, "field_5": " " }, "geometry": { "type": "Point", "coordinates": [ -0.9781, 51.4543 ] } }
+]
+}
